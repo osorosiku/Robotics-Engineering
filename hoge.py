43972042b0aa0220d23c8b5131bc98263ffbfce0
osorosiku <sagasyunta@gmail.com>
2026-07-16 14:12:33 +0900

    motor test

--- hoge.py
+++ hoge.py
@@ -1,8 +1,10 @@
-from gpiozero import LED
+from gpiozero import Motor
 from signal import pause
 
-led = LED(17)
+# input1（GPIO17）にプラス、input2（GPIO18）にマイナスを流す
+motor = Motor(forward=17, backward=18)
 
-led.on()
-
-pause()
+# モーターを後進方向に回転
+while True:
+    motor.forward(1)
+  
