b5647ccc9b0af5f0584ff9d3e963ff496a808549
a24i054 <a24i054kd@ous.jp>
2026-07-30 15:41:12 +0900

    aa

--- controller.py
+++ controller.py
@@ -97,22 +97,22 @@ while True:
    turn = joystick.get_axis(0)
 
    # 入力によっての動作管理
-   if around <= -0.5:
-      forward()
-   elif around >= 0.5:
-      backward()
-   elif turn <= -0.5:
-      left()
-   elif turn >= 0.5:
-      right()
-   elif around <= -0.2 and turn <= -0.2:
+   if around <= -0.2 and turn <= -0.2:
       left_forward()
    elif around <= -0.2 and turn >= 0.2:
       right_forward()
    elif around >= 0.2 and turn <= -0.2:
       left_backward()
    elif around >= 0.2 and turn >= 0.2:
-      right_backward()   
+      right_backward()
+   elif around <= -0.5:
+      forward()
+   elif around >= 0.5:
+      backward()
+   elif turn <= -0.5:
+      left()
+   elif turn >= 0.5:
+      right()  
    else:
       stop()
    # CPU使用率を抑えるために短い待機を入れる(負荷対策であった方が良いらしい)
